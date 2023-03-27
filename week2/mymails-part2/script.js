43247f86b23ba4663a9367984270f2ec7e6aa620
Your name <Your email>
2023-03-27 10:49:18 +0200

    maj mymails p2

--- week2/mymails-part2/script.js
+++ week2/mymails-part2/script.js
@@ -1,1 +1,12 @@
 // Insert your code here
+document.querySelector('#msg-container').innerHTML += `
+        <div class="row">
+          <img class="avatar" src="images/avatar-1.jpg" />
+          <div class="text-container">
+            <h6>Valentin Lemaitre</h6>
+            <p>
+             Bonjour j'apprends à programmer
+            </p>
+          </div>
+          <span class="delete">✖</span>
+        </div> `  
