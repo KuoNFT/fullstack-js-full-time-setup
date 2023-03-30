1edc6d3c406298f47d3cbf980cf37286b984f6e0
Your name <Your email>
2023-03-30 16:39:10 +0200

    apiscraper

--- week2/apiscraping/app.js
+++ week2/apiscraping/app.js
@@ -3,29 +3,39 @@ const db = require('./database/setup');
 // Do not edit/remove code above this line
 
 // Insert your code here
-db.deleteMany().then((result) => {
-    console.log(result)
-    // Reset de la base de données
-    //db.deleteMany()
-
-  
+// Supprimer toutes les entrées dans la base de données
+db.deleteMany()
+  .then(() => {
     // Fetch des fruits depuis l'API et stockage dans la base de données
-    
-  fetch('https://fruityvice.com/api/fruit/all')
+    fetch('https://fruityvice.com/api/fruit/all')
       .then(response => response.json())
       .then(data => {
         // Stocker les fruits dans une variable
         const fruits = data;
-  
-        // Faire quelque chose avec la variable fruits
-        for (let i = 0; i < fruits.length; i++) {
-          const newFruit = new db(fruits[i]);
-          newFruit.save();
-        }
-      });
-  }
-)
-  //fetchFruits();
-  
+
+        // Créer un tableau pour stocker les fruits à insérer
+        const fruitsToInsert = [];
+
+        // Pour chaque fruit dans la liste, créer une nouvelle instance de Fruit
+        fruits.forEach(fruit => {
+          const newFruit = new db({
+            name: fruit.name,
+            family: fruit.family,
+            genus: fruit.genus,
+            order: fruit.order,
+            nutritions: fruit.nutritions,
+            id: fruit.id // Ajouter la propriété "id" manquante
+          });
+          fruitsToInsert.push(newFruit);
+        });
+
+        // Insérer tous les fruits dans la base de données en une seule fois
+        db.insertMany(fruitsToInsert)
+          .then(() => console.log('La base de données a été mise à jour avec succès !'))
+          .catch(error => console.error(error));
+      })
+      .catch(error => console.error(error));
+  })
+  .catch(error => console.error(error));
  
 
