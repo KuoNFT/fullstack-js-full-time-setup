ab444ea8c8a35ac7f7db59f5e8f3eba409be8739
Your name <Your email>
2023-03-30 12:24:06 +0200

    blackboard

--- week2/blackboard-part1/app.js
+++ week2/blackboard-part1/app.js
@@ -1,23 +1,62 @@
 const db = require('./database/setup'); // Do not edit/remove this line
 
 
-function displayAllArticles() {}
+function displayAllArticles() {
+	db.find().then( data => {
+		console.log(data)
+	})
+}
+displayAllArticles()
+
+function displayArticleByName(articleName) {
+	db.find({name:`${articleName}`}).then( data => {
+		console.log(data)
+	})
+}
+
+displayArticleByName('NKD')
+
+function displayArticleByID(articleId) {
+	db.find({_id:`${articleId}`}).then( data => {
+		console.log(data)
+	})
+}
+
+displayArticleByID('525ea9ca38d6a3776994651e')
+
+function updateArticlePrice(articleId, newPrice) {
+	db.updateOne({_id:`${articleId}`},{price:`${newPrice}`})
+	.then(()=> {
+		db.find().then(data=> {
+			console.log(data)
+		})
+		
+	} )
+
+}
+
+updateArticlePrice('525ea9ca38d6a3776994651e', '400')
+
+function updateArticleStock(articleId, newStock) {
+	db.updateOne({_id:`${articleId}`},{stock:`${newStock}`})
+	.then(()=> {
+		db.find().then(data=> {
+			console.log(data)
+		})
+		
+	} )
+}
+
+updateArticleStock('525ea9ca38d6a3776994651e', "2")
 
 
-function displayArticleByName(articleName) {}
+function resetStocks() {
+	db.updateMany({},{stock: '0'}).then(data => {
+		console.log(data)
+	})
+}
 
-
-function displayArticleByID(articleId) {}
-
-
-function updateArticlePrice(articleId, newPrice) {}
-
-
-function updateArticleStock(articleId, newStock) {}
-
-
-function resetStocks() {}
-
+resetStocks()
 
 // Do not edit/remove code under this line
 module.exports = {
