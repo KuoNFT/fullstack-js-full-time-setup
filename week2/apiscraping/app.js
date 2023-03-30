2b17b08ef3698929aed7701c2dc06834f804e8dc
Your name <Your email>
2023-03-30 15:43:32 +0200

    apiscraper

--- week2/apiscraping/app.js
+++ week2/apiscraping/app.js
@@ -4,30 +4,29 @@ const db = require('./database/setup');
 
 // Insert your code here
 
-function fetchFruits() {
+function FetchFruits() {
+    // Reset de la base de données
+    db.deleteMany({}, err => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Base de données réinitialisée.');
+      }
+    });
+  
+    // Fetch des fruits depuis l'API et stockage dans la base de données
     fetch('https://fruityvice.com/api/fruit/all')
       .then(response => response.json())
       .then(data => {
-        // Supprimer les fruits existants de la collection
-        db.collection.deleteMany({}, () => {
-          // Ajouter les fruits récupérés à partir de l'API à la collection
-          db.collection.insertMany(data, (error, result) => {
-            if (error) {
-              console.error(error);
-            } else {
-              console.log(`${result.insertedCount} nouveaux fruits ont été ajoutés à la collection.`);
-            }
-          });
-        });
-      })
-      .catch(error => console.error(error));
+        // Stocker les fruits dans une variable
+        const fruits = data;
+  
+        // Faire quelque chose avec la variable fruits
+        for (let i = 0; i < fruits.length; i++) {
+          const newFruit = new db(fruits[i]);
+          newFruit.save();
+        }
+      });
   }
   
   fetchFruits();
-
-  
-  
-  
-  
-  
-  
