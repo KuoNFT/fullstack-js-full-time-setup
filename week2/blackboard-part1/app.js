f4d64d5529df8bd71a540977c2f340e03bcc12d2
Your name <Your email>
2023-03-30 13:01:54 +0200

    bboard

--- week2/blackboard-part1/app.js
+++ week2/blackboard-part1/app.js
@@ -18,9 +18,7 @@ function displayArticleByName(articleName) {
 //displayArticleByName('NKD')
 
 function displayArticleByID(articleId) {
-	db.findOne({_id: articleId}).then(data => {
-		console.log(data);
-	});
+	db.findOne({_id: articleId})
 }
 
 
@@ -46,9 +44,7 @@ function updateArticleStock(articleId, newStock) {
 
 
 function resetStocks() {
-	db.updateMany({},{stock: '0'}).then(data => {
-		console.log(data);
-	});
+	db.updateMany({},{stock: '0'});
 }
 
 //resetStocks()
