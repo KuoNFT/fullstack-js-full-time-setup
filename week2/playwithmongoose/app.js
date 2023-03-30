0f91963b353737f68e8d9faccd83f1cc7c0108ab
Your name <Your email>
2023-03-30 11:48:43 +0200

    playwithmongoose

--- week2/playwithmongoose/app.js
+++ week2/playwithmongoose/app.js
@@ -15,11 +15,22 @@ createTodo('touch Grass', '1')
 
 
 // Complete one todo by name (update property done = true)
-function completeTodo(name) {}
+function completeTodo(name) {
+    db.updateOne(
+        {name: `${name}`},
+        {done : true}
+    )
+}
+
+completeTodo('Walk the dog')
 
 
 // Delete one todo by name
-function deleteTodo(name) {}
+function deleteTodo(name) {
+    db.deleteOne(
+        {name: `${name}`}
+    )
+}
 
 
 module.exports = { createTodo, completeTodo, deleteTodo }; // Do not edit/remove this line
