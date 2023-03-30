3b4ab8caf8a266fbf89da8265a4b58d407dca381
Your name <Your email>
2023-03-30 14:28:50 +0200

    apiscraping

--- week2/apiscraping/app.js
+++ week2/apiscraping/app.js
@@ -3,3 +3,19 @@ const db = require('./database/setup');
 // Do not edit/remove code above this line
 
 // Insert your code here
+//https://fruityvice.com/api/fruit/all 
+//On veut fetch tous les fruits disponibles sur l'api fuityvice et la renvoyer dans notre database 
+const newUser = new db({
+    firstName: 'Jane',
+    lastName: 'Doe',
+   });
+   
+   newUser.save().then(() => {
+    
+    db.find().then(data => {
+      console.log(data);
+    });
+   
+   });
+
+   newUser()
--- week2/apiscraping/connection.js
+++ week2/apiscraping/connection.js
@@ -1,4 +1,4 @@
 // Insert your connection string inside this variable
-const connectionString = 'mongodb+srv://.../apiscraping';
+const connectionString = 'mongodb+srv://valentinlemaitre:4gcrIN1cbB3BjFbh@cluster0.pi5jnq3.mongodb.net/apiscraping';
 
 module.exports = connectionString; // Do not edit/remove this line
