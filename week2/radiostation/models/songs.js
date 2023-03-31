8a8667ce8769633b4faf230c061b372aec556111
Your name <Your email>
2023-03-31 10:58:34 +0200

    radiostation

--- week2/radiostation/models/songs.js
+++ week2/radiostation/models/songs.js
@@ -2,10 +2,16 @@
 // Insert your code here
 const mongoose = require('mongoose')
 
-const userSchema = mongoose.Schema({
+const newRadioSongSchema = mongoose.Schema({
     annoying : Boolean,
     artist : String,
     creationDate : Date,
     title : String, 
 
 })
+
+// Insert your code here
+
+const Songs = mongoose.model("songs", newRadioSongSchema)
+
+module.exports = Songs;
