74133f1a9bf65ead499815ccc068a88d8efbd360
Your name <Your email>
2023-03-31 16:21:20 +0200

    miniUNO

--- week2/miniuno2/models/connection.js
+++ week2/miniuno2/models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://valentinlemaitre:4gcrIN1cbB3BjFbh@cluster0.pi5jnq3.mongodb.net/miniUno';
+const connectionString = 'mongodb+srv://valentinlemaitre:4gcrIN1cbB3BjFbh@cluster0.pi5jnq3.mongodb.net/MiniUno';
 ;
 
 mongoose.connect(connectionString, {connectTimeoutMS: 2000})
