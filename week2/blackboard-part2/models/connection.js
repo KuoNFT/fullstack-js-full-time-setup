804f186dbbac69aae589e408741e0a5202b45b4f
Your name <Your email>
2023-03-31 11:18:46 +0200

    bboardP2

---
+++ week2/blackboard-part2/models/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connectionString = "mongodb+srv://valentinlemaitre:4gcrIN1cbB3BjFbh@cluster0.pi5jnq3.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(connectionString, {connectTimeoutMS: 2000})
+.then(()=> console.log('Database Connected'))
+.catch(error => console.log(error))
