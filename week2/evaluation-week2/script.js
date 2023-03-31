6d813f837c5ab7203931c968bf5a8f75f9556f2e
Your name <Your email>
2023-03-31 14:56:04 +0200

    EvalWeek2

--- week2/evaluation-week2/script.js
+++ week2/evaluation-week2/script.js
@@ -5,19 +5,22 @@
 //puis on assigne les valeurs questions et punchline 
 //aux balises result-question et result-punchine en modifiant leur textContent
 //
-
-const fetchBtn = document.getElementById("fetch-btn");
+const fetchBtn = document.querySelector("#fetch-btn");
 
 fetchBtn.addEventListener("click", () => {
-  fetch("https://backend-omega-seven.vercel.app/api/getjoke")
-  .then((response)=> response.json())
+
+ fetch("https://backend-omega-seven.vercel.app/api/getjoke")
+ .then((response)=> response.json())
   .then((data)=> {
-    const resultQuestion = data.question
+    console.log(data)
+    const resultQuestion = data[0].question
+    console.log(data.question)
     console.log(resultQuestion)
-    const resultPunchline = data.punchline
+    const resultPunchline = data[0].punchline
     document.querySelector('#result-question').textContent = resultQuestion
     document.querySelector('#result-punchline').textContent = resultPunchline
 
   })
 });
+
 
