37ef5295167bb180fc434a6e03befe1fcb0404b8
Your name <Your email>
2023-03-31 14:28:27 +0200

    evalweek2

--- week2/evaluation-week2/script.js
+++ week2/evaluation-week2/script.js
@@ -1,1 +1,21 @@
 // Write your code here
+
+// On va : créer un eventListener click sur le bouton #fetch-btn.
+//A l'intérieur de ce fetch on va fetch auprès de l'API https://backend-omega-seven.vercel.app/api/getjoke
+//puis on assigne les valeurs questions et punchline 
+//aux balises result-question et result-punchine en modifiant leur textContent
+//
+
+const fetchBtn = document.getElementById("fetch-btn");
+
+fetchBtn.addEventListener("click", () => {
+  fetch("https://backend-omega-seven.vercel.app/api/getjoke")
+  .then((response)=> response.json())
+  .then((data)=> {
+    const resultQuestion = data.question
+    const resultPunchline = data.resultPunchline
+    document.querySelector('#result-question').textContent = resultQuestion
+    document.querySelector('#result-punchline').textContent = resultPunchline
+
+  })
+});
