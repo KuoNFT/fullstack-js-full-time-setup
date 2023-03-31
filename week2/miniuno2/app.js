4d49cc26d5f88d46b354c00cfe227d9a4ab5bb09
Your name <Your email>
2023-03-31 17:07:08 +0200

    miniuno

--- week2/miniuno2/app.js
+++ week2/miniuno2/app.js
@@ -5,80 +5,48 @@ const Play = require('./models/plays')
 /////////////////////////////////////////////
 
 
-//function playUno(cards, lastPlay) {
-//    let lastCard = lastPlay;
-//    let cardsToPlay = [];
-
- 	
-//    if (cards.length > 0){
-//    for (let i = 0; i < cards.length; i++) {
-//        if (cards[i].color === lastCard.color || cards[i].number === lastCard.number) {
-//            cardsToPlay.push(cards[i]);
-//			cards.slice(i, 1)
-//        }
-//    }
-
-//    lastCard = cardsToPlay[0];
-//	const playedCard = new Play({
-//		color: lastCard.color,
-//		number: lastCard.number,
-//	})
-//	playedCard.save()
-
-
-//    return lastCard;
-//} else {
-//	console.log('Game Over')
-//}
-//}
-
-function playUno(cards, lastPlay) {
-    Play.deleteMany({}, function(err) {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log('Plays collection reset before starting the game');
+async function playUno(cards, lastPlay) {
+    await Play.deleteMany();
+    console.log('Plays collection reset before starting the game');
+  
     let lastCard = lastPlay;
-
+  
     while (cards.length > 0) {
-        let cardsToPlay = [];
-
-        for (let i = 0; i < cards.length; i++) {
-            if (cards[i].color === lastCard.color || cards[i].number === lastCard.number) {
-                cardsToPlay.push(cards[i]);
-                cards.splice(i, 1);
-                break;
-            }
+      let cardsToPlay = [];
+  
+      for (let i = 0; i < cards.length; i++) {
+        if (cards[i].color === lastCard.color || cards[i].number === lastCard.number) {
+          cardsToPlay.push(cards[i]);
+          cards.splice(i, 1);
+          break;
         }
-
-        if (cardsToPlay.length > 0) {
-            lastCard = cardsToPlay[0];
-            const playedCard = new Play({
-                color: lastCard.color,
-                number: lastCard.number,
-            });
-            playedCard.save();
-        } else {
-            console.log('No valid cards to play');
-            break;
-        }
+      }
+  
+      if (cardsToPlay.length > 0) {
+        lastCard = cardsToPlay[0];
+        const playedCard = new Play({
+          color: lastCard.color,
+          number: lastCard.number,
+        });
+        await playedCard.save();
+      } else {
+        console.log('No valid cards to play');
+        break;
+      }
     }
-
+  
     console.log('Game Over');
-})}
-
-
-
-const cardsExample = [
-	{ color: 'green', number: 6 },
-	{ color: 'red', number: 6 },
-	{ color: 'red', number: 9 },
-	{ color: 'green', number: 9 },
-	{ color: 'yellow', number: 9 },
-];
-const lastPlayExample = { color: 'blue', number: 6 };
-
-playUno(cardsExample, lastPlayExample);
-
-module.exports = playUno; // Do not edit/remove this line
+  }
+  
+  const cardsExample = [
+    { color: 'green', number: 6 },
+    { color: 'red', number: 6 },
+    { color: 'red', number: 9 },
+    { color: 'green', number: 9 },
+    { color: 'yellow', number: 9 },
+  ];
+  const lastPlayExample = { color: 'blue', number: 6 };
+  
+  playUno(cardsExample, lastPlayExample);
+  
+  module.exports = playUno; // Do not edit/remove this line
