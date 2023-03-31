763d504e499c2b0f36e23d0ee31b7bbf10a51080
Your name <Your email>
2023-03-31 11:37:14 +0200

    bboard2

--- week2/blackboard-part2/models/connection.js
+++ week2/blackboard-part2/models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = "mongodb+srv://valentinlemaitre:4gcrIN1cbB3BjFbh@cluster0.pi5jnq3.mongodb.net/test";
+const connectionString = "mongodb+srv://valentinlemaitre:4gcrIN1cbB3BjFbh@cluster0.pi5jnq3.mongodb.net/users?retryWrites=true&w=majority";
 
 mongoose.connect(connectionString, {connectTimeoutMS: 2000})
 .then(() => console.log('Database Connected'))
