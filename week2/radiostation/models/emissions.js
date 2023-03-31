5bb411fbc5da46019b936c4205ab3e0f7c96fd87
Your name <Your email>
2023-03-31 10:22:27 +0200

    radiostation

--- week2/radiostation/models/emissions.js
+++ week2/radiostation/models/emissions.js
@@ -1,1 +1,12 @@
 // Insert your code here
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    discontinued : Boolean,
+    mainAnimator : String,
+    name : String,
+    nbAnimators : number, 
+
+})
+
+const Emissions = mongoose.model('emissions', userSchema)
