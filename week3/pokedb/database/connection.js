e8bf68776b6b255093efdce3a553e2d3ccfaf2d3
Your name <Your email>
2023-04-03 15:48:01 +0200

    pokedb

--- week3/pokedb/database/connection.js
+++ week3/pokedb/database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // Insert your connection string inside this variable
-const connectionString = 'mongodb+srv://.../pokeDB';
+const connectionString = 'mongodb+srv://valentinlemaitre:4gcrIN1cbB3BjFbh@cluster0.pi5jnq3.mongodb.net/pokeDB'
 
 mongoose.set("strictQuery", true); // Remove Mongoose warning in console
 
--- week3/pokedb/modules/subDocuments.js
+++ week3/pokedb/modules/subDocuments.js
@@ -17,18 +17,25 @@ function createPokemon(pokemonName, typeName, typeColor) {
 //! Add a sub-document/sub-object to an array
 function addType(pokemonName, typeName, typeColor) {
   // Write your code here
+  Pokemon.updateOne({name: pokemonName}, {$push: {types: typeName, color: typeColor}})
+  .then((data) => console.log(data) )
 }
 // addType('pikachu', 'rock', 'brown')
 
 //! Update type in "pokemon" collection
 function updateType(typeName, typeColor) {
+  Pokemon.updateMany({types: {$elemMatch: {name: typeName}}}, {$set: {'types.$.color': typeColor}})
+  .then((data) => console.log(data))
+  }
   // Write your code here
-}
+
 // updateType('rock', 'grey')
 
 //! Delete type from "pokemon" collection
 function deleteType(typeName) {
   // Write your code here
+  Pokemon.updateMany({types: {$elemMatch: {name: typeName}}}, {$pull: {types: {name: typeName}}})
+  .then((data)=> console.log(data))
 }
 // deleteType('rock')
 
