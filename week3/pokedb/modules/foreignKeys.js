253a2a0972240925db01563f37a543ac6832cb7c
Your name <Your email>
2023-04-03 17:12:06 +0200

    pokedb

--- week3/pokedb/modules/foreignKeys.js
+++ week3/pokedb/modules/foreignKeys.js
@@ -14,33 +14,35 @@ function addType(typeName, typeColor) {
 //! Create new pokemon document/object in 'pokemons' collection
 function createPokemon(pokemonName, ObjectId) {
   // Write your code here
-  let newPokemon = new Pokemon({
+  let newPokemon = new Pokemon_V2({
     name: pokemonName,
     types: ObjectId,
   })
   newPokemon.save().then((data)=> console.log('New Pokemon created'))
 }
- createPokemon('pikachu', '642ae0aa817c412c41bbc754') //you can get an _id from a type document in database
+ //createPokemon('pikachu', '642ae0aa817c412c41bbc754') //you can get an _id from a type document in database
 
 //! Display all pokemons from database
 function displayPokemons() {
-  Pokemon.findMany()
+  Pokemon_V2.find()
+  .populate('types')
+  .then(data=>{console.log(data)})
 	// Write your code here
 }
-// displayPokemons();
+//displayPokemons();
 
 //! Update a type document/object in 'types' collection
-function updateType(typeName, typeColor) {}
-//  Type.updateOne({name: typeName},{color: typeColor} )
+function updateType(typeName, typeColor) {
+Type.updateOne({name: typeName},{color: typeColor} )
   // Write your code here
-//}
+}
 // updateType('rock', 'grey')
 
 //! Delete a type document/object in 'types' collection
-function deleteType(typeName) {}
+function deleteType(typeName) {
   Type.deleteOne({name: typeName})
   // Write your code here
-//}
+}
 // deleteType('rock')
 
 // Do not edit/remove the code below this line !
