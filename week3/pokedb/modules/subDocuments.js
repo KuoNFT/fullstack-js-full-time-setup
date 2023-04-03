c17cbaf84051d78d9535aa668eaf46bc706c9fe0
Your name <Your email>
2023-04-03 16:12:07 +0200

    pokedb

--- week3/pokedb/modules/subDocuments.js
+++ week3/pokedb/modules/subDocuments.js
@@ -10,15 +10,20 @@ displayPokemons();
 
 //! Create a new pokemon in database
 function createPokemon(pokemonName, typeName, typeColor) {
-  // Write your code here
-}
-// createPokemon('pikachu', 'electric', 'yellow')
+  // Créer un nouveau Pokemon
+  let newPokemon = new Pokemon({
+    name: pokemonName,
+    types: [{ name: typeName, color: typeColor }],
+  })
+  newPokemon.save().then((data)=> console.log("New Pokemon created"))
+};
+ createPokemon('pikachu', 'electric', 'yellow')
 
 //! Add a sub-document/sub-object to an array
 function addType(pokemonName, typeName, typeColor) {
-  // Write your code here
-  Pokemon.updateOne({name: pokemonName}, {$push: {types: typeName, color: typeColor}})
-  .then((data) => console.log(data) )
+  Pokemon.updateOne({ name: pokemonName }, {
+    $push: { types: { name: typeName, color: typeColor } }
+  }).then((data) => console.log(data))
 }
 // addType('pikachu', 'rock', 'brown')
 
