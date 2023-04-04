5ec599bb3e6bad9e4383573750a3c14fd7f65f25
Your name <Your email>
2023-04-04 15:12:21 +0200

    weatheraoppP1

--- week3/expressmiddleware/app.js
+++ week3/expressmiddleware/app.js
@@ -13,6 +13,16 @@ app.use(cors());
 // authorization middleware
 const apiKeys = ['azerty123', 'supersecretapikey'];
 
+function validateApiKey(req, res, next) {
+    const vapiKey = req.headers['api-key'];
+    if (validApiKeys.includes(vapiKey)) {
+      next(); // Si la clé est valide, on passe au middleware suivant
+    } else {
+      res.status(403).json({result: false, message: 'Invalid API key'}); // Si la clé est invalide, on renvoie une réponse avec un statut 403 et un message d'erreur
+    }
+  }
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
