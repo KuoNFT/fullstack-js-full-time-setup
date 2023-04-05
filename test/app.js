9b148ad98b9b93dcc9b41a0bb35c94cd292ae4bc
Your name <Your email>
2023-04-05 10:09:40 +0200

    04/04

--- test/app.js
+++ test/app.js
@@ -22,8 +22,22 @@ function sumEvenNum() {
     }
     console.log(isEven);
   }
+ //sumEvenNum();
+
+
+//fonction pour trier des nombres dans une array :
+function getRangeTable(minVal, maxVal) {
+    const list = [];
+    for (let i = minVal; i <= maxVal; i++) {
+        list.push(i);
+    }
+    return list;
+}
+
+console.log(getRangeTable(-5, 5));
+// Expected: [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
   
-  //sumEvenNum();
+ 
 
 //Fonction Nombre premier 
 function nombresPremiers(number) {
@@ -57,6 +71,59 @@ function nombresPremiers(number) {
   }
 //isPalindrome('benjamin')
 
+//Fonction retrouver les palindromes dans une array : 
+function getPalindromes(words) {
+    // Initialiser une liste vide pour stocker les palindromes
+    const palindromes = [];
+    
+    // Parcourir chaque mot dans le tableau
+    for (let i = 0; i < words.length; i++) {
+    // Inverser le mot et comparer avec le mot original
+    const reversed = words[i].split('').reverse().join('');
+    if (words[i] === reversed) {
+    // Si le mot est un palindrome, l'ajouter à la liste
+    palindromes.push(words[i].toUpperCase());
+    }
+    }
+    
+    // Trier la liste de palindromes par ordre alphabétique
+    palindromes.sort();
+    
+    return palindromes;
+    }
+
+    const examplePalindromes = ['HELLO', 'KAYAK', 'ANNA', 'TOTO', 'JAVASCRIPT','LOL', "MODEM"];
+    console.log(getPalindromes(examplePalindromes));
+    // Expected: ['ANNA', 'KAYAK', LOL']
+
+//Générer tous les anngrames d'un mot 
+function anagramMe(word) {
+    const anagramList = [];
+    
+    if (word.length === 1) {
+        return [word];
+    }
+     
+    for (let i = 0; i < word.length; i++){
+        const letter = word[0];
+        const newWord = anagramMe(word.slice(1, word.length));
+        
+        for (let j = 0; j < newWord.length; j++) {
+            anagramList.push(letter + newWord[j]);
+        }
+        
+        word = word.substr(1, word.length - 1) + letter;
+    }
+    
+    return anagramList.sort();
+}
+
+console.log(anagramMe('AGE'));
+// Expected: ['AEG', 'AGE', 'EAG', 'EGA', 'GAE', 'GEA']
+
+
+    
+
 //fonction pour trier un tableau d'entiers dans l'ordre croissant/décroissant.
 
 function isSorted(array) {
@@ -72,3 +139,227 @@ function isSorted(array) {
   }
   
   console.log(isSorted([340, 54, 32, 233, 122])); // [340, 233, 122, 54, 32]
+
+
+  //To roman number
+
+  function toRomanNumerals(number) {
+    const arabicValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const romanValues = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    let romanValue = '';
+    
+    for (let i = 0; i < arabicValues.length; i++) {
+      while (number >= arabicValues[i]) {
+        romanValue += romanValues[i];
+        number -= arabicValues[i];
+      }
+    }
+    
+    return romanValue;
+  }
+  
+  console.log(toRomanNumerals(984)); // Expected: 'CMLXXXIV'
+
+
+  //Suite de fibo 
+
+  function fibonacci(n) {
+    let result = [0, 1];
+    if (n <= 2) {
+    return result.slice(0, n);    
+  }
+ 
+ let first = 0;
+ let second = 1;
+ let next;
+  
+ //Ecrivez votre code ici
+for(let i = 2; i < n; i++){
+next = first + second;
+first = second;
+second = next;
+result.push(next);
+}
+ return result;
+}
+
+console.log(fibonacci(2))
+
+//fonction pour remplacer les champs vides dans une liste d'objets par null 
+
+function checkValues(data) {
+    let newList = [];
+    for (let i = 0; i < data.length; i++) {
+    let user = data[i];
+    for (let key in user) {
+      if (user[key] === '' || user[key] === ' ') {
+        user[key] = null;
+      }
+    }
+    newList.push(user);
+  }
+
+  return newList;
+}
+
+const list = [
+   { fullname: 'John Doe', age: 42, tel: '', email: ' ' },
+   { fullname: 'Vanessa Williams', age: '', tel: ' ', email: 'v.williams@gmail.com' },
+   { fullname: 'Richard Lawson', age: 42, tel: '0765432109', email: '' },
+];
+console.log(checkValues(list));
+// Expected:
+// [{ fullname: 'John Doe', age: 42, tel: null, email: null },
+//     { fullname: 'Vanessa Williams', age: null, tel: null, email: 'v.williams@gmail.com' },
+//     { fullname: 'Richard Lawson', age: 42, tel: '0765432109', email: null }]
+
+
+//retrouver un éléments dans une array :
+
+function findUser(request, list) {
+    let userFound = false;
+    for(let i = 0; i < list.length; i++){
+  if (list.find(request => request === list[i]) === request ){
+      userFound = true;
+  }
+        
+    }
+    // Insert your code here
+    
+   return userFound;
+}
+
+const search = 'Lucas';
+const usersList = ['Lucas', 'Juliette', 'William', 'Camille', 'Pauline', 'Sébastien'];
+
+console.log(findUser(search, usersList));
+// Expected: true
+
+
+//Mini UNO : simule un tour de UNO, la première carte qui correspond est jouée :
+
+function getFirstCardToPlay(cards, lastPlay) {
+    // Insert your code here
+    // on veut itérer dans cards et regarder si lastplay color ou last play number correspond à une de nos cartes. La première qui correspond prend la place lastPlay 
+    for(let i = 0; i <cards.length; i++){
+        const card = cards[i];
+        if(card.color === lastPlay.color || card.number === lastPlay.color){
+        
+        return card;
+            
+        }
+    }
+    return null
+}
+
+const cardsExample = [
+    { color: 'green', number: 6 },
+    { color: 'red', number: 6 },
+    { color: 'red', number: 9 },
+    { color: 'green', number: 9 },
+    { color: 'yellow', number: 9 },
+];
+const lastPlayExample = { color: 'blue', number: 6 };
+
+console.log(JSON.stringify(getFirstCardToPlay(cardsExample, lastPlayExample)));
+
+
+//Fonction calcul d'un cashback => Le tableau ordersList recense un exemple d’achats effectués par un client sur différents sites partenaires, avec un cashback exprimé en pourcentage (ex: 5 vaut 5 %).
+//Le cashback est un montant rétribué au client, venant alimenter une cagnotte dont il peut disposer comme il le souhaite.
+
+function cashback(data){
+    let total = 0;
+  
+    for (const order of data) {
+      const cashbackAmount = order.total * order.cashback / 100;
+      total += cashbackAmount;
+    }
+    
+    return total.toFixed(2)
+  }
+  
+  const ordersList = [
+     {partner: 'Carrefour', total: 57.45, cashback: 5},
+     {partner: 'Amazon', total: 60.20, cashback: 3},
+     {partner: 'Boulanger', total: 149, cashback: 6},
+     {partner: 'Fnac', total: 84.15, cashback: 4},
+  ];
+  console.log(cashback(ordersList)); // Output: 16.98
+
+
+  //Savoir si tous les élèments dans une liste sont les mêmes :
+
+  function checkDifferences(arrays) {
+    let result = [];
+ for(let i =0; i< arrays.length; i++){
+     let firstElement = arrays[i][0];
+     let same = true;
+     
+ 
+ for(let j=0; j < arrays[i].length; j++){
+     if(arrays[i][j] !== firstElement){
+         same = false;
+     }
+     }
+  result.push(same)
+     
+ }
+    // Insert your code here
+
+    return result;
+}
+
+console.log(checkDifferences([
+    [true, true, true, true],
+    ['test', 'test', 'test'],
+    [1, 1, 1, 2],
+    ['10', 10, 10, 10]
+]));
+// Expected: true, true, false, false
+
+//À partir du tableau à deux dimensions, recréez un nouveau tableau des nombres les plus grands de chaque série.
+
+function getBiggest(numbers) {
+    let bigNumbers = [];
+  
+    for (let i = 0; i < numbers.length; i++) {
+      let biggestNumber = numbers[i][0];
+      for (let j = 1; j < numbers[i].length; j++) {
+        if (numbers[i][j] > biggestNumber) {
+          biggestNumber = numbers[i][j];
+        }
+      }
+      bigNumbers.push(biggestNumber);
+    }
+  
+    return bigNumbers;
+  }
+  
+  let list = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]];
+  console.log(getBiggest(list)); // [5,27,39,1001]
+
+  //Un candidat passe un test de 10 questions, dont les bonnes réponses sont stockées dans le tableau correctAnswers.
+//Les réponses du candidat sont quant à elles stockées dans le tableau results.
+
+function isSucceeded(ans, res) {
+    let success = false;
+    let score = 0;
+    for (let i =0; i < ans.length; i++){
+        if(ans[i] === res[i]){
+           score++
+        }
+    }
+    score = (score/ans.length)*100
+    if (score >= 50){
+        success = true
+    }
+    // Insert your code here On va devoir comparer deux tableaux. Il faut que index[i] === index[j]. Si c'est le cas alors il faut push pour ajouter +1 à score. Si après la comparaison score > 5 alors success = true
+    
+    
+    return { success: success, score: score };
+}
+
+const correctAnswers = [2, 3, 4, 1, 2, 2, 4, 3];
+const results = [2, 4, 3, 1, 3, 2, 4, 3];
+console.log(isSucceeded(correctAnswers, results));
+// Expected: {"success":true,"score":62.5}
