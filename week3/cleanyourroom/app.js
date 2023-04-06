f22ec64c33479ae58f2fcec9f7f67478916f63f6
Your name <Your email>
2023-04-06 10:33:24 +0200

    cleanyourroom

--- week3/cleanyourroom/app.js
+++ week3/cleanyourroom/app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var adminRouter = require('../routes/admin')
+var moderatorRouter = require('../routes/moderator')
 var indexRouter = require('./routes/index');
 
 var app = express();
@@ -15,7 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
+app.use('/admin', adminRouter);
+app.use('/moderator', moderatorRouter);
+
 
 module.exports = app;
---
+++ week3/cleanyourroom/routes/moderator.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.post('/moderator/login', (req, res) => {
+    const password = 'modpwd123';
+    if (req.body.password === password) {
+      res.json({ result: true });
+    }
+    else {
+      res.json({ result: false, error: 'Invalid password for moderator' });
+    }
+  });
+  
+  
+  
+  
+  router.post('/moderator/secretaction', (req, res) => {
+    const password = 'modpwd123';
+    if (req.body.password === password) {
+      res.json({ result: true });
+    }
+    else {
+      res.json({ result: false, error: 'Invalid password for moderator' });
+    }
+  });
+  
+  
+  
+  
+  
