9dff7153b20e5bfbc79650210687d6b73a7783a0
Your name <Your email>
2023-04-06 11:47:09 +0200

    weatherappp3

--- week3/weatherapp-part3/backend/routes/weather.js
+++ week3/weatherapp-part3/backend/routes/weather.js
@@ -3,57 +3,78 @@ var router = express.Router();
 const fetch = require("node-fetch");
 const City = require('../models/cities')
 
+const OWM_API_KEY = 'c88d94bbb2c9195835e71452811315ca'
 
 
-router.post("/", (req, res) => {
-    if (!weather.some(e => e.cityName.toLowerCase() === req.body.cityName.toLowerCase())) {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${req.body.cityName}&appid=${OWM_API_KEY}&units=metric`)
-        .then(response => response.json())
-        .then(apiData => {
-          const newCity = {
-            cityName: apiData.name,
-            main: apiData.weather[0].main,
-            description: apiData.weather[0].description,
-            tempMin: apiData.main.temp_min,
-            tempMax: apiData.main.temp_max,
-          };
+router.post("/weather", (req, res) => {
+    const cityName = req.body.cityName.toLowerCase();
+    
+    // Vérifier si la ville est déjà dans la base de données
+    City.findOne({ cityName }, (err, city) => {
+        if (err) {
+            return res.json({ result: false, error: err.message });
+        }
+        
+        if (city) {
+            return res.json({ result: false, error: "City already saved" });
+        }
+        
+        // Si la ville n'est pas dans la base de données, récupérer les données depuis l'API OpenWeatherMap
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${OWM_API_KEY}&units=metric`)
+            .then(response => response.json())
+            .then(apiData => {
+                const newCity = new City({
+                    cityName: apiData.name.toLowerCase(),
+                    main: apiData.weather[0].main,
+                    description: apiData.weather[0].description,
+                    tempMin: apiData.main.temp_min,
+                    tempMax: apiData.main.temp_max,
+                });
+
+                newCity.save((err, city) => {
+                    if (err) {
+                        return res.json({ result: false, error: err.message });
+                    }
+                    
+                    res.json({ result: true, weather: city });
+                });
+            })
+            .catch(err => {
+                res.json({ result: false, error: err.message });
+            });
+    });
+});
   
-          weather.push(newCity);
-          res.json({ result: true, weather: newCity });
-        });
-    } else {
-      res.json({ result: false, error: "City already saved" });
-    }
+  router.get("/weather", (req, res) => {
+    City.find().then(data => {
+     res.json({ weather : data });
+    })
+    
   });
   
-  router.get("/", (req, res) => {
-    res.json({ weather });
-  });
+
+  router.get('/weather/:cityName', (req, res) => {
+    City.findOne({ cityName: { $regex: new RegExp(req.params.cityName, 'i') } }).then(data => {
+            if (data) {
+                    res.json({ result: true, weather: data });
+            } else {
+                    res.json({ result: false, error: 'City not found' });
+            }
+    });
+});
   
-  router.get("/:cityName", (req, res) => {
-    const searchedWeather = weather.find(
-      e => e.cityName.toLowerCase() === req.params.cityName.toLowerCase()
-    );
-    if (searchedWeather) {
-      res.json({ result: true, weather: searchedWeather });
-    } else {
-      res.json({ result: false, error: "City not found" });
-    }
-  });
-  
-  router.delete("/weather/:cityName", (req, res) => {
-    const searchedWeather = weather.find(
-      e => e.cityName.toLowerCase() === req.params.cityName.toLowerCase()
-    );
-    if (searchedWeather) {
-      weather = weather.filter(
-        e => e.cityName.toLowerCase() !== req.params.cityName.toLowerCase()
-      );
-      res.json({ result: true, weather });
-    } else {
-      res.json({ result: false, error: "City not found" });
-    }
-  });
+router.delete("/weather/:cityName", (req, res) => {
+    const cityName = req.params.cityName;
+    City.findOneAndDelete({ cityName }).then(deletedCity => {
+        if (deletedCity) {
+            res.json({ result: true, deletedCity });
+        } else {
+            res.json({ result: false, error: "City not found" });
+        }
+    }).catch(err => {
+        res.json({ error: err.message });
+    });
+});
 
 
   
