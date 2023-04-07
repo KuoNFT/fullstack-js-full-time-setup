2e5012b497bc364d36d0681770fd79485ca85f75
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-07 19:25:28 +0200

    blabla

--- week3/weatherapp-part4/backend/urls.js
+++ week3/weatherapp-part4/backend/urls.js
@@ -2,6 +2,6 @@
 const BACKEND_URL = 'https://backendweatherapp.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'https://frontendweatherappdef.vercel.app';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
