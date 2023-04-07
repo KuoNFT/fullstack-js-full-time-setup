59a86aaffc9c0a6fc90e9e64074751750f2d1ff6
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-07 18:50:04 +0200

    blabla

--- week3/weatherapp-part4/backend/models/connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectionString = 'mongodb+srv://valentinlemaitre:4gcrIN1cbB3BjFbh@cluster0.pi5jnq3.mongodb.net/weatherapp';
-
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log('Database connected'))
-  .catch(error => console.error(error));
