8d0aaec75ece795880f3a8a79138bdee48829139
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-07 14:02:45 +0200

    deploy

--- week3/deploymetovercel/urls.js
+++ week3/deploymetovercel/urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = 'https://backend-woad-three.vercel.app/';
+const BACKEND_URL = 'https://backend-woad-three.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = 'https://frontendwhatyear.vercel.app/';
+const FRONTEND_URL = 'https://backend-kuonft.vercel.app';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
