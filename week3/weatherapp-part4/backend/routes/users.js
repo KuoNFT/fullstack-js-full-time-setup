1bc0960b28b4644e193b06d900e3b683a2bf236f
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-07 17:46:19 +0200

    weatherapp

--- week3/weatherapp-part4/backend/routes/users.js
+++ week3/weatherapp-part4/backend/routes/users.js
@@ -7,48 +7,43 @@ const { checkBody } = require('../modules/checkBody');
 
 router.post('/signup', (req, res) => {
   const expectedKeys = ['name', 'email', 'password'];
-  if (!checkBody(req.body, expectedKeys)) {
-    res.status(400).json({ result: false, error: 'Invalid request body' });
-    return;
+  if (checkBody(req.body, expectedKeys)) {
+    User.findOne({ email: req.body.email }).then(data => {
+      if (data === null) {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        });
+   
+        newUser.save().then(newDoc => {
+          res.json({ result: true });
+        });
+      } else {
+        res.json({ result: false, error: 'User already exists' });
+      }
+    });
+  } else {
+    res.json({ result: false, error: 'missing or empty fields' });
   }
-
-  // Check if the user has not already been registered
-  User.findOne({ email: req.body.email }).then(data => {
-    if (data === null) {
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      });
-
-      newUser.save().then(newDoc => {
-        res.json({ result: true });
-      });
-    } else {
-      // User already exists in database
-      res.json({ result: false, error: 'User already exists' });
-    }
-  });
 });
 
 router.post('/signin', (req, res) => {
   const expectedKeys = ['email', 'password'];
-  if (!checkBody(req.body, expectedKeys)) {
-    res.status(400).json({ result: false, error: 'Invalid request body' });
-    return;
+  if (checkBody(req.body, expectedKeys)) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email, password }).then(data => {
+      if (data === null) {
+        res.json({ result: false, error: 'User not found' });
+      } else {
+        res.json({ result: true });
+      }
+    });
+  } else {
+    res.json({ result: false, error: 'missing or empty fields' });
   }
-
-  const email = req.body.email;
-  const password = req.body.password;
-
-  User.findOne({ email, password }).then(data => {
-    if (data === null) {
-      res.json({ result: false, error: 'User not found' });
-    } else {
-      res.json({ result: true });
-    }
-  });
 });
-
 
 module.exports = router;
