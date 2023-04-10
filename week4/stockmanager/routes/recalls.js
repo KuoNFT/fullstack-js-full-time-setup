d1ab65432da665c0a7815cd38bda6cbd24523e28
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-10 14:45:38 +0200

    stockmanager

--- week4/stockmanager/routes/recalls.js
+++ week4/stockmanager/routes/recalls.js
@@ -2,4 +2,42 @@ var express = require('express');
 var router = express.Router();
 const app = express();
 const productsData = require('../data');
-module.exports = router;
+
+
+router.get('/recalls/byBrand/:brand', (req, res) => {
+    const recalls = productsData
+      .filter(p => p.brand === req.params.brand)
+      .reduce((acc, curr) => {
+        curr.batches.forEach(batch => {
+          if (batch.recall) {
+            acc.push({ id: curr.id, batchId: batch.id });
+          }
+        });
+        return acc;
+      }, []);
+    if (recalls.length > 0) {
+      res.json(recalls);
+    } else {
+      res.status(404).send('No recalls found for this brand');
+    }
+  });
+
+  router.get('/recalls/byTimestamp/:timestamp', (req, res) => {
+    const recalls = productsData
+      .reduce((acc, curr) => {
+        curr.batches.forEach(batch => {
+          if (batch.recall && new Date(batch.expirationDate).getTime() === Number(req.params.timestamp)) {
+            acc.push({ id: curr.id, batchId: batch.id });
+          }
+        });
+        return acc;
+      }, []);
+    if (recalls.length > 0) {
+      res.json(recalls);
+    } else {
+      res.status(404).send('No recalls found for this timestamp');
+    }
+  });
+  
+
+  module.exports = router;
