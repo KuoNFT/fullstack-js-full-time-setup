e3172b7969b3ca9ad5904a55bdce8d98bd88d3f6
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-13 13:23:49 +0200

    mymoviz

--- week4/mymoviz-part1/components/Home.js
+++ week4/mymoviz-part1/components/Home.js
@@ -1,15 +1,45 @@
 import styles from '../styles/Home.module.css';
+import { Button, Popover } from 'antd';
+import Movie from './Movie';
+import 'antd/dist/antd.css';
 
 function Home() {
-  return (
+  const favMovies = ['Movie 1', 'Movie 2', 'Movie 3', 'Movie 4'];
+
+  const content = (
     <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+      {favMovies.map((movie, index) => (
+        <p key={index}>{movie}</p>
+      ))}
+    </div>
+  );
+
+  const moviesList = [];
+  for (let i = 0; i< 10; i++) {
+    moviesList.push(<Movie key={i}/>)
+  }
+
+  return (
+    <div className="home">
+    <header className = {styles.header}>
+      <div className={styles.logocontainer}>
+        <img src="logo.png" alt="Logo" />
+        <img className={styles.logo} src="logoletter.png" alt="Letter logo" style={{ height: '100%', objectFit: 'contain' }}/>
+      </div>
+      <div className={styles.favorites}>
+        <Popover className={styles.popover} content={content} title="Liked movies">
+          <Button type="primary">♥ 4 movies</Button>
+        </Popover>
+      </div>
+    </header>
+    <div className = 'content'>
+        <h2>Last releases</h2>
+        {moviesList}
+    </div>
+    
     </div>
   );
 }
+
 
 export default Home;
---
+++ week4/mymoviz-part1/components/Movie.js
@@ -0,0 +1,30 @@
+import styles from '../styles/Movie.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+
+function Movie () { 
+
+    const voteCount = 7;
+    const stars = [];
+    for (let i = 0; i < 10; i++) {
+      if (i < voteCount) {
+        stars.push(<FontAwesomeIcon key={i} icon={faStar} />);
+      } else {
+        stars.push(<FontAwesomeIcon key={i} icon={faStar} color="#ccc" />);
+      }
+    }
+return (
+    <div className={styles.card}>
+      <img className={styles.image} src='poster.jpg' alt='poster'/>
+      <div className={styles.textContainer}>
+        <span className={styles.name}>Name</span>
+        <p className={styles.description}>Descrption</p>
+        <span className={styles.vote}>{stars} ({voteCount} votes)</span>
+      </div>
+    </div>
+   );
+
+}
+
+export default Movie;
