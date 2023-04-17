6793f922b058f2a688b91d9048c3a16cc3d9783c
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-17 15:32:06 +0200

    yamsp2

--- week5/yams-part2/components/Dice.js
+++ week5/yams-part2/components/Dice.js
@@ -1,9 +1,22 @@
+
+
+import { useState } from 'react';
 import styles from '../styles/Dice.module.css';
 
 function Dice(props) {
+  const [value, setValue] = useState(props.number);
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    const newValue = Math.floor(Math.random() * 6) + 1;
+    setValue(newValue);
+    setCount(count + 1);
+  }
+
   return (
     <div>
-      <img className={styles.dice} src={`${props.number}.png`} alt={props.number} />
+      <img className={styles.dice} src={`${value}.png`} alt={value} onClick={handleClick} />
+      <div className={styles.count}>{count}</div>
     </div>
   );
 }
--- week5/yams-part2/components/Home.js
+++ week5/yams-part2/components/Home.js
@@ -1,32 +1,59 @@
+import { useState } from 'react';
 import Dice from './Dice';
 import styles from '../styles/Home.module.css';
 
 function Home() {
-  const dices = [];
-  for (let i = 0; i < 5; i++) {
-    dices.push(<Dice key={i} number={i + 1} />);
-  }
+const [gameStarted, setGameStarted] = useState(false);
+const [diceValues, setDiceValues] = useState([1, 1, 1, 1, 1]);
 
-  return (
-    <div className={styles.container}>
-      <div>
-        <div className={styles.triangleLeft}></div>
-        <div className={styles.triangleLeftRed}></div>
-        <div className={styles.triangleLeft}></div>
-      </div>
-      <div className={styles.middleSection}>
-        <img src="logo.png" alt="Logo" className={styles.logo} />
-        <div className={styles.board}>
-          {dices}
-        </div>
-      </div>
-      <div>
-        <div className={styles.triangleRight}></div>
-        <div className={styles.triangleRightRed}></div>
-        <div className={styles.triangleRight}></div>
-      </div>
-    </div>
-  );
+const handleLaunchClick = () => {
+setDiceValues(getRandomDiceValues());
+setGameStarted(true);
+};
+
+const getRandomDiceValues = () => {
+return [
+getRandomValue(),
+getRandomValue(),
+getRandomValue(),
+getRandomValue(),
+getRandomValue(),
+];
+};
+
+const getRandomValue = () => {
+return Math.floor(Math.random() * 6) + 1;
+};
+
+const dices = diceValues.map((value, index) => (
+<Dice key={index} number={value} alt={value.toString()} />
+));
+
+return (
+<div className={styles.container}>
+<div>
+<div className={styles.triangleLeft}></div>
+<div className={styles.triangleLeftRed}></div>
+<div className={styles.triangleLeft}></div>
+</div>
+<div className={styles.middleSection}>
+<img src="logo.png" alt="Logo" className={styles.logo} />
+<div className={styles.board}>
+{gameStarted && dices}
+</div>
+{!gameStarted && (
+<button className={styles.launchBtn} onClick={handleLaunchClick}>
+Lancer les dés
+</button>
+)}
+</div>
+<div>
+<div className={styles.triangleRight}></div>
+<div className={styles.triangleRightRed}></div>
+<div className={styles.triangleRight}></div>
+</div>
+</div>
+);
 }
 
 export default Home;
