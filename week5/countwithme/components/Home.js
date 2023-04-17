04f536b22bb09d1d1b56adfbf2b87b22fed1e994
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-17 10:03:25 +0200

    countwithme

--- week5/countwithme/components/Home.js
+++ week5/countwithme/components/Home.js
@@ -1,16 +1,27 @@
 import styles from '../styles/Home.module.css';
+import { useState } from 'react';
 
 function Home() {
-  return (
-    <div className={styles.container}>
-      <img className={styles.logo} src="logo.png" />
-      <div className={styles.counterContainer}>
-        <button className={styles.decrementBtn} id="decrementBtn">-</button>
-        <span className={styles.counter} id="counter">{counter}</span>
-        <button className={styles.incrementBtn} id="incrementBtn">+</button>
-      </div>
-    </div>
-  );
+const [counter, setCounter] = useState(0);
+
+const handleIncrement = () => {
+setCounter(counter + 1);
+};
+
+const handleDecrement = () => {
+setCounter(counter - 1);
+};
+
+return (
+<div className={styles.container}>
+<img className={styles.logo} src="logo.png" />
+<div className={styles.counterContainer}>
+<button className={styles.decrementBtn} id="decrementBtn" onClick={() => handleDecrement()}>-</button>
+<span className={styles.counter} id="counter">{counter}</span>
+<button className={styles.incrementBtn} id="incrementBtn" onClick={() => handleIncrement()}>+</button>
+</div>
+</div>
+);
 }
 
 export default Home;
