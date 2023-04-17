ab21bccf57b69f76110c395bd286a4c2737093ee
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-17 11:09:41 +0200

    mymoviz

--- week5/mymoviz-part3/components/Movie.js
+++ week5/mymoviz-part3/components/Movie.js
@@ -1,27 +1,54 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faCamera } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/Movie.module.css';
 
 function Movie(props) {
-  const stars = [];
-  for (let i = 0; i < 10; i++) {
-    let style = {};
-    if (i < props.voteAverage - 1) {
-      style = { 'color': '#f1c40f' };
-    }
-    stars.push(<FontAwesomeIcon key={i} icon={faStar} style={style} />);
-  }
+const [personalNote, setPersonalNote] = useState(0);
+const [watchCount, setWatchCount] = useState(0);
+const [like, setLike] = useState(false);
 
-  return (
-    <div className={styles.card}>
-      <img className={styles.image} src={props.poster} alt={props.title} />
-      <div className={styles.textContainer}>
-        <span className={styles.name}>{props.title}</span>
-        <p className={styles.description}>{props.overview}</p>
-        <span className={styles.vote}>{stars} ({props.voteCount})</span>
-      </div>
+
+
+const stars = [];
+for (let i = 0; i < 10; i++) {
+let style = {};
+if (i < props.voteAverage - 1) {
+style = { 'color': '#f1c40f' };
+}
+stars.push(<FontAwesomeIcon key={i} icon={faStar} style={style} />);
+}
+
+const personalStars = [];
+for (let i = 0; i < 10; i++) {
+let style = {};
+if (i < personalNote) {
+style = { 'color': '#2196f3'};
+}
+personalStars.push(<FontAwesomeIcon key={i} icon={faStar} onClick={() => setPersonalNote(i + 1)} className="note" style={style} />);
+}
+
+
+const watchStyle = watchCount > 0 ? { color: '#e74c3c' } : {};
+
+const handleWatchClick = () => {
+  setWatchCount(watchCount + 1);
+};
+
+
+return (
+  <div className={styles.card}>
+    <img className={styles.image} src={props.poster} alt={props.title} />
+    <div className={styles.textContainer}>
+      <span className={styles.name}>{props.title}</span>
+      <p className={styles.description}>{props.overview}</p>
+      <span className={styles.vote}>{stars} ({props.voteCount})</span>
+      <div className={styles.personalStars}>{personalStars}</div>
+      <FontAwesomeIcon icon={faCamera} className="watch" style={watchStyle} onClick={handleWatchClick} />
+      <span>({watchCount})</span>
     </div>
-  );
+  </div>
+);
 }
 
 export default Movie;
