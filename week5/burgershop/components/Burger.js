b8f528da96caf20547c1c4c479e85dc6cb6ed095
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-18 10:14:00 +0200

    burgershop

--- week5/burgershop/components/Burger.js
+++ week5/burgershop/components/Burger.js
@@ -1,15 +1,24 @@
 import styles from '../styles/Burger.module.css';
 
 function Burger(props) {
-	return (
-		<div className={styles.container}>
-			<h2 className={styles.name}>{props.name}</h2>
-			<img className={styles.images} src={props.image} />
-			<div className={styles.btnContainer}>
+  const handleClickSelect = () => {
+    props.selectBurger(props.name);
+  }
 
-			</div>
-		</div>
-	);
+  const handleClickRemove = () => {
+    props.removeBurger(props.name);
+  }
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.name}>{props.name}</h2>
+      <img className={styles.images} src={props.image} />
+      <div className={styles.btnContainer}>
+        <button className={styles.selectBtn} onClick={handleClickSelect}>Select</button>
+        <button className={styles.removeBtn} onClick={handleClickRemove}>Remove all</button>
+      </div>
+    </div>
+  );
 }
 
 export default Burger;
--- week5/burgershop/components/Home.js
+++ week5/burgershop/components/Home.js
@@ -3,6 +3,15 @@ import Burger from './Burger';
 import styles from '../styles/Home.module.css';
 
 function Home() {
+  const [selectedBurgers, setSelectedBurgers] = useState([]);
+
+  const selectBurger = (newBurger) => {
+    setSelectedBurgers([...selectedBurgers, newBurger]);
+  }
+
+  const removeBurger = (burgerToDelete) => {
+    setSelectedBurgers(selectedBurgers.filter(burger => burger !== burgerToDelete));
+  }
 
   const burgersData = [
     { name: 'The New Yorker', image: 'newyorker.jpg' },
@@ -11,16 +20,17 @@ function Home() {
   ];
 
   const burgers = burgersData.map((data, i) => {
-    return <Burger key={i} name={data.name} image={data.image} />;
+    return <Burger key={i} name={data.name} image={data.image} selectBurger={selectBurger} removeBurger={removeBurger} />;
   });
 
   return (
     <div className={styles.container}>
       <img src="logo.png" className={styles.logo} />
-      <h4 className={styles.text}>Number of burgers selected: <span id="counter"></span></h4>
+      <h4 className={styles.text}>Number of burgers selected: {selectedBurgers.length}</h4>
       <div className={styles.burgerContainer}>
         {burgers}
       </div>
+      <p>Hi All, Explore <a href="https://drimmake.com/">https://drimmake.com/</a> for learn more.</p>
     </div>
   );
 }
