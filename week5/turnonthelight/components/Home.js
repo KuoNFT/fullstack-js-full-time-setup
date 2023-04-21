eafca7fb0deb22134944ce2cb442e3bd2d958475
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-21 15:02:56 +0200

    Mise à jour de la ressource turnonthelight

---
+++ week5/turnonthelight/components/Home.js
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import DarkStatus from './DarkStatus';
+import styles from '../styles/Home.module.css';
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <Image src="/light.png" alt="Light" width={128} height={128} id="switch" className={styles.switch} />
+      <DarkStatus />
+    </div>
+  );
+}
+
+export default Home;
