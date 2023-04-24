e60ab209d313d85366e63f220d483ecfe2590c70
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-24 12:34:25 +0200

    mn

--- week6/morningnews-part3/frontend/components/Header.js
+++ week6/morningnews-part3/frontend/components/Header.js
@@ -31,7 +31,7 @@ function Header() {
   const [errorMessage, setErrorMessage] = useState('');
 
   async function handleSignUp() {
-    const response = await fetch("/signup", {
+    const response = await fetch("http://localhost:3000/users/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -53,7 +53,7 @@ function Header() {
   }
 
   async function handleSignIn() {
-    const response = await fetch("/signin", {
+    const response = await fetch("http://localhost:3000/users/signin", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
