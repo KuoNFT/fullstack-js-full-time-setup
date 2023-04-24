52c256adb47a0f691221605aea55f36d862a5dfa
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-24 09:41:12 +0200

    cialogin

--- week6/cialogin/components/Home.js
+++ week6/cialogin/components/Home.js
@@ -1,7 +1,22 @@
+import React, { useState } from 'react';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 function Home() {
+  const [agentId, setAgentId] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    if (agentId === 'codybanks' && password === 'qwerty123') {
+      setIsLoggedIn(true);
+    }
+  };
+
+  if (isLoggedIn) {
+    return <div>Welcome, agent Cody Banks.</div>;
+  }
+
   return (
     <div className={styles.container}>
       <Image src="/logo.png" alt="Logo" width={200} height={200} />
@@ -9,15 +24,27 @@ function Home() {
       <h1>CIA LOGIN</h1>
       <div className={styles.inputContainer}>
         <span>AGENT ID</span>
-        <input type="text" id="agentId" />
+        <input
+          type="text"
+          id="agentId"
+          value={agentId}
+          onChange={(e) => setAgentId(e.target.value)}
+        />
       </div>
 
       <div className={styles.inputContainer}>
         <span>PASSWORD</span>
-        <input type="password" id="password" />
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
 
-      <button id="login">LOGIN</button>
+      <button id="login" onClick={handleLogin}>
+        LOGIN
+      </button>
     </div>
   );
 }
