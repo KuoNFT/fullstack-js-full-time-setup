e18afaf5f0c0f4348ea57f0f5aac9f855aac1865
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-25 11:37:41 +0200

    morningnews

--- week6/morningnews-part4/backend/routes/users.js
+++ week6/morningnews-part4/backend/routes/users.js
@@ -7,52 +7,49 @@ const { checkBody } = require('../modules/checkBody');
 const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
 
-const saltRounds= 10;
 
 router.post('/signup', (req, res) => {
-	if (!checkBody(req.body, ['username', 'password'])) {
+  if (!checkBody(req.body, ['username', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
   }
-  const hash = bcrypt.hashSync(req.body.password, 10)
-  
+ 
   // Check if the user has not already been registered
   User.findOne({ username: req.body.username }).then(data => {
     if (data === null) {
-      const token = uid2(32);
-      const hashedPassword = bcrypt.hashSync(req.body.password, saltRounds);
-  
+      const hash = bcrypt.hashSync(req.body.password, 10);
+ 
       const newUser = new User({
         username: req.body.username,
-        password: hashedPassword,
-        token: token
+        password: hash,
+        token: uid2(32),
+        canBookmark: true,
       });
-  
-      newUser.save().then(() => {
-        res.json({ result: true });
+ 
+      newUser.save().then(newDoc => {
+        res.json({ result: true, token: newDoc.token });
       });
     } else {
       // User already exists in database
       res.json({ result: false, error: 'User already exists' });
     }
   });
-});
-
-router.post('/signin', (req, res) => {
+ });
+ 
+ router.post('/signin', (req, res) => {
   if (!checkBody(req.body, ['username', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
   }
-  
-
-  User.findOne({ username: req.body.username}).then(data => {
-    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+ 
+  User.findOne({ username: req.body.username }).then(data => {
+    if (bcrypt.compareSync(req.body.password, data.password)) {
       res.json({ result: true, token: data.token });
     } else {
-      res.json({ result: false, error: 'User not found' });
+      res.json({ result: false, error: 'User not found or wrong password' });
     }
   });
-});
+ });
 
 router.get('/users/canBookmark/:token', (req, res) => {
   User.findOne({ token: req.params.token }).then(data => {
