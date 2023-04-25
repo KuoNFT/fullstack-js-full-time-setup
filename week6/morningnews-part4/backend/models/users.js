86ae8d9a410e7cf93132d07b25c8bfab8da9fee1
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-25 10:44:25 +0200

    morningnews

--- week6/morningnews-part4/backend/models/users.js
+++ week6/morningnews-part4/backend/models/users.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   username: String,
   password: String,
+  token: String,
+  canBookmark: Boolean,
+  
 });
 
 const User = mongoose.model('users', userSchema);
--- week6/morningnews-part4/backend/routes/users.js
+++ week6/morningnews-part4/backend/routes/users.js
@@ -4,19 +4,26 @@ var router = express.Router();
 require('../models/connection');
 const User = require('../models/users');
 const { checkBody } = require('../modules/checkBody');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
 
 router.post('/signup', (req, res) => {
 	if (!checkBody(req.body, ['username', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
   }
+  
+  const hash = bcrypt.hashSync(req.body.password, 10)
 
   // Check if the user has not already been registered
   User.findOne({ username: req.body.username }).then(data => {
     if (data === null) {
+      const token = uid2(32)
+      const hash = bcrypt.hashSync(req.body.password)
       const newUser = new User({
         username: req.body.username,
-        password: req.body.password,
+        password: hash,
+        token:  token,
       });
 
       newUser.save().then(() => {
@@ -34,14 +41,26 @@ router.post('/signin', (req, res) => {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
   }
+  
 
-  User.findOne({ username: req.body.username, password: req.body.password }).then(data => {
-    if (data) {
-      res.json({ result: true });
+  User.findOne({ username: req.body.username}).then(data => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token });
     } else {
       res.json({ result: false, error: 'User not found' });
     }
   });
 });
+
+router.get('/users/canBookmark/:token', (req, res) => {
+  User.findOne({ token: req.params.token }).then(data => {
+    if (data) {
+      res.json({ result: true, canBookmark: data.canBookmark });
+    } else {
+      res.json({ result: false, error: 'User not found' });
+    }
+  });
+});
+
 
 module.exports = router;
