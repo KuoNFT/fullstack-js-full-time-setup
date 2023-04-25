b9ae90592080477e1fe79139dc213912985d5f3b
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-25 15:53:05 +0200

    google

--- week6/googleconnect/components/Home.js
+++ week6/googleconnect/components/Home.js
@@ -1,14 +1,42 @@
+import { useState } from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+import jwtDecode from 'jwt-decode';
 import styles from '../styles/Home.module.css';
 
 function Home() {
+  const [user, setUser] = useState(null);
+
+  const handleSuccess = (response) => {
+    const decodedToken = jwtDecode(response.credential);
+    const { name, email } = decodedToken;
+    setUser({ name, email });
+  };
+
+  const handleError = (error) => {
+    console.error('Erreur lors de la connexion Google :', error);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h1>Welcome</h1>
-        <h2>Log in to you Google account to continue</h2>
-        <div className={styles.divider}></div>
-        {/* Add Google connect here */}
+        {user ? (
+          <>
+            <h1>Bienvenue, {user.name}</h1>
+            <h2>{user.email}</h2>
+          </>
+        ) : (
+          <>
+            <h1>Bienvenue</h1>
+            <h2>Connectez-vous à votre compte Google pour continuer</h2>
+            <div className={styles.divider}></div>
+            <GoogleLogin
+              clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+              onSuccess={handleSuccess}
+              onFailure={handleError}
+              buttonText="Se connecter avec Google"
+            />
+          </>
+        )}
       </div>
     </div>
   );
--- week6/googleconnect/pages/_app.js
+++ week6/googleconnect/pages/_app.js
@@ -1,13 +1,16 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App({ Component, pageProps }) {
   return (
     <>
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
       <Head>
         <title>Google connect</title>
       </Head>
       <Component {...pageProps} />
+      </GoogleOAuthProvider>
     </>
   );
 }
