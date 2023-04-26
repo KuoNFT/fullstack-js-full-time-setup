de85372f8dcedd83dfbd8628bb40820d92dd44ca
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-26 11:46:54 +0200

    tt

--- week6/timetracker/pages/_app.js
+++ week6/timetracker/pages/_app.js
@@ -1,15 +1,42 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import {Provider} from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import {configureStore} from 'reduxjs/toolkit';
+import activitiesReducer from '../reducers/activities';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import { combineReducers } from 'redux';
 
-function App({ Component, pageProps }) {
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  activities: activitiesReducer,
+});
+
+const persistedReducer= persistReducer(store);
+
+
+const store= configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Time tracker</title>
       </Head>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
     </>
   );
 }
-
-export default App;
