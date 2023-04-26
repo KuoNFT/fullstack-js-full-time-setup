80be635bcfdc3cfa897d64f5f4f7560641aa0190
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-26 17:20:57 +0200

    SW

--- week6/themewarswithtailwind/components/Home.js
+++ week6/themewarswithtailwind/components/Home.js
@@ -29,8 +29,9 @@ function Home() {
 return (
     <div id="main" className={`py-0 px-5 bg-left-top bg-right-top bg-no-repeat bg-cover w-screen min-h-screen relative flex flex-col justify-center items-center transition-all ${theme === 'light' ? 'bg-light' : 'bg-dark'}`}>
       {/* TOGGLE THEME BUTTON */}
-      <div className="rounded-full cursor-pointer border-none absolute top-6 right-6 w-[50px] h-[50px] bg-neutral-500">
-        <button onClick={toggleTheme} className="flex justify-center items-center w-full h-full rounded-full -translate-y-[6px] active:-translate-y-[2px] bg-neutral-800">
+      <div className={`${theme=== 'light' ? 'bg-neutral-500' : 'bg-neutral-800'} rounded-full cursor-pointer border-none absolute top-6 right-6 w-[50px] h-[50px]`}>
+
+        <button onClick={toggleTheme} className={`${theme === "light" ? "bg-neutral-800" : "bg-neutral-500"} flex justify-center items-center w-full h-full rounded-full -translate-y-[6px] active:-translate-y-[2px]`}>
         {theme === 'light'
       ? <FontAwesomeIcon icon={faSith} className={`${theme === 'light' ? 'text-neutral-200' : 'text-neutral-800'} text-2x1` } />
       : <FontAwesomeIcon icon={faJedi} className={`${theme === 'light' ? 'text-neutral-800' : 'text-neutral-200'} text-2x1`} />}
@@ -39,7 +40,7 @@ return (
 
       
       {/* TITLE */}
-      <h1 className={`text-6xl m-10 font-starjedi ${theme === 'light' ? 'text-neutral-800' : 'text-neutral-200'}`}>THEME WARS</h1>
+      <h1 className={`text-6xl m-10 font-starjedi ${theme === 'light' ? 'text-neutral-800' : 'text-neutral-200'} text-2x1`}>THEME WARS</h1>
 
 
       {/* SEARCH */}
