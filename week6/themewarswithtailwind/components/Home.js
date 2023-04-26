2b41e21df251b617b5fc0c691349d9ed02f39cdc
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-26 15:13:00 +0200

    SW

--- week6/themewarswithtailwind/components/Home.js
+++ week6/themewarswithtailwind/components/Home.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import {useTheme} from 'next-themes'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSith } from '@fortawesome/free-brands-svg-icons';
 import { faJedi } from '@fortawesome/free-solid-svg-icons';
@@ -8,7 +7,7 @@ import Card from './Card';
 import Form from './Form';
 
 function Home() {
-  const {theme, setTheme}= useTheme()
+  const [theme, setTheme] = useState('light');
   const [searchResult, setSearchResult] = useState([]);
 
   const handleSearch = async (searchText) => {
@@ -21,15 +20,22 @@ function Home() {
   });
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light')
-  }
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
 
- 
-return (
-    <div id="main" className={`py-0 px-5 bg-left-top bg-right-top bg-no-repeat bg-cover w-screen min-h-screen relative flex flex-col justify-center items-center transition-all ${theme === 'light' ? 'bg-light' : 'bg-dark'}`}>
+  const themeStyles = theme === 'light' ? {
+    backgroundColor: '#ffffff',
+    color: '#333333'
+  } : {
+    backgroundColor: '#1a1a1a',
+    color: '#e5e5e5'
+  };
+
+  return (
+    <div id="main" className="py-0 px-5 bg-left-top bg-right-top bg-no-repeat bg-cover w-screen min-h-screen relative flex flex-col justify-center items-center transition-all" style={themeStyles}>
       {/* TOGGLE THEME BUTTON */}
       <div className="rounded-full cursor-pointer border-none absolute top-6 right-6 w-[50px] h-[50px] bg-neutral-500">
-        <button onClick={toggleTheme} className="flex justify-center items-center w-full h-full rounded-full -translate-y-[6px] active:-translate-y-[2px] bg-neutral-800">
+        <button className="flex justify-center items-center w-full h-full rounded-full -translate-y-[6px] active:-translate-y-[2px] bg-neutral-800" onClick={toggleTheme}>
           {theme === 'light'
             ? <FontAwesomeIcon icon={faSith} className="text-2xl text-neutral-200" />
             : <FontAwesomeIcon icon={faJedi} className="text-2xl text-neutral-200" />}
@@ -37,7 +43,7 @@ return (
       </div>
 
       {/* TITLE */}
-      <h1 className="text-6xl m-10 text-neutral-800 ">THEME WARS</h1>
+      <h1 className="text-6xl m-10 text-neutral-800" style={{ fontFamily: 'Starjedi', color: theme === 'light' ? '#333333' : '#e5e5e5' }}>THEME WARS</h1>
 
       {/* SEARCH */}
       <Form handleSearch={handleSearch} />
