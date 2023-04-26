a09cdea0f7fdd66d2161f0950422a1f04dd10150
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-26 15:31:23 +0200

    SW

--- week6/themewarswithtailwind/components/Home.js
+++ week6/themewarswithtailwind/components/Home.js
@@ -1,36 +1,51 @@
 import { useState } from 'react';
-import { useTheme } from 'next-themes';
-// Les autres importations
+import {useTheme} from 'next-themes'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSith } from '@fortawesome/free-brands-svg-icons';
+import { faJedi } from '@fortawesome/free-solid-svg-icons';
+import { searchAPI } from '../functions/searchAPI';
+import Card from './Card';
+import Form from './Form';
 
 function Home() {
+  const {theme, setTheme}= useTheme()
   const [searchResult, setSearchResult] = useState([]);
-  const { theme, setTheme } = useTheme();
 
-  const handleThemeToggle = () => {
-    setTheme(theme === 'dark' ? 'light' : 'dark');
+  const handleSearch = async (searchText) => {
+    const result = await searchAPI(searchText);
+    setSearchResult(result);
   };
 
-  // Les autres fonctions et le rendu
-  return (
-    <div
-      id="main"
-      className="py-0 px-5 bg-left-top bg-right-top bg-no-repeat bg-cover w-screen min-h-screen relative bg-light dark:bg-dark flex flex-col justify-center items-center transition-all"
-    >
+  const cards = searchResult.map((data, i) => {
+    return <Card key={i} infos={data} />;
+  });
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
+ 
+return (
+    <div id="main" className={`py-0 px-5 bg-left-top bg-right-top bg-no-repeat bg-cover w-screen min-h-screen relative flex flex-col justify-center items-center transition-all ${theme === 'light' ? 'bg-light' : 'bg-dark'}`}>
       {/* TOGGLE THEME BUTTON */}
       <div className="rounded-full cursor-pointer border-none absolute top-6 right-6 w-[50px] h-[50px] bg-neutral-500">
-        <button
-          onClick={handleThemeToggle}
-          className="flex justify-center items-center w-full h-full rounded-full -translate-y-[6px] active:-translate-y-[2px] bg-neutral-800"
-        >
-          {theme === 'light' ? (
-            <FontAwesomeIcon icon={faSith} className="text-2xl text-neutral-200" />
-          ) : (
-            <FontAwesomeIcon icon={faJedi} className="text-2xl text-neutral-200" />
-          )}
+        <button onClick={toggleTheme} className="flex justify-center items-center w-full h-full rounded-full -translate-y-[6px] active:-translate-y-[2px] bg-neutral-800">
+          {theme === 'light' 
+            ?( <FontAwesomeIcon icon={faSith} className="text-2xl text-neutral-200" />)
+            : (<FontAwesomeIcon icon={faJedi} className="text-2xl text-neutral-200" />)}
         </button>
       </div>
 
-      {/* Les autres éléments */}
+      {/* TITLE */}
+      <h1 className="text-6xl m-10 text-neutral-800 ">THEME WARS</h1>
+
+      {/* SEARCH */}
+      <Form handleSearch={handleSearch} />
+
+      {/* CARDS */}
+      <div className="w-2/5 flex flex-col items-center">
+        {cards}
+      </div>
     </div>
   );
 }
