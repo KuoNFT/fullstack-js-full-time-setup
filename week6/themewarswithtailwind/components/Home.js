6bcdd6ff7039394710b11f530b9372eed67e4135
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-26 12:44:50 +0200

    Mise à jour de la ressource themewarswithtailwind

---
+++ week6/themewarswithtailwind/components/Home.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSith } from '@fortawesome/free-brands-svg-icons';
+import { faJedi } from '@fortawesome/free-solid-svg-icons';
+import { searchAPI } from '../functions/searchAPI';
+import Card from './Card';
+import Form from './Form';
+
+function Home() {
+  const theme = 'light';
+  const [searchResult, setSearchResult] = useState([]);
+
+  const handleSearch = async (searchText) => {
+    const result = await searchAPI(searchText);
+    setSearchResult(result);
+  };
+
+  const cards = searchResult.map((data, i) => {
+    return <Card key={i} infos={data} />;
+  });
+
+  return (
+    <div id="main" className="py-0 px-5 bg-left-top bg-right-top bg-no-repeat bg-cover w-screen min-h-screen relative bg-light flex flex-col justify-center items-center transition-all">
+      {/* TOGGLE THEME BUTTON */}
+      <div className="rounded-full cursor-pointer border-none absolute top-6 right-6 w-[50px] h-[50px] bg-neutral-500">
+        <button className="flex justify-center items-center w-full h-full rounded-full -translate-y-[6px] active:-translate-y-[2px] bg-neutral-800">
+          {theme === 'light'
+            ? <FontAwesomeIcon icon={faSith} className="text-2xl text-neutral-200" />
+            : <FontAwesomeIcon icon={faJedi} className="text-2xl text-neutral-200" />}
+        </button>
+      </div>
+
+      {/* TITLE */}
+      <h1 className="text-6xl m-10 text-neutral-800">THEME WARS</h1>
+
+      {/* SEARCH */}
+      <Form handleSearch={handleSearch} />
+
+      {/* CARDS */}
+      <div className="w-2/5 flex flex-col items-center">
+        {cards}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
