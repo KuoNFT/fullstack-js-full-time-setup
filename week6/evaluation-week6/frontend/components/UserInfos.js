26da032e956c325396dec9de5c7a198b27c6bd87
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-04-28 12:58:33 +0200

    eval-week6

--- week6/evaluation-week6/frontend/components/UserInfos.js
+++ week6/evaluation-week6/frontend/components/UserInfos.js
@@ -1,8 +1,8 @@
-import { useSelector } from 'react-redux'; // Importez useSelector
+import { useSelector } from 'react-redux'; 
 import styles from '../styles/UserInfos.module.css';
 
 function UserInfos() {
-	// Utilisez useSelector pour lire les données du reducer
+	
 	const userInfos = useSelector((state) => state.user.value);
 
 	return (
@@ -10,13 +10,13 @@ function UserInfos() {
 			<h4 className={styles.title}>
 				My username is in the redux store:
 				<span className={styles.userInfos}>
-					{userInfos.username /* Affichez le username du store */}
+					{userInfos.username}
 				</span>
 			</h4>
 			<h4 className={styles.title}>
 				My token is in the redux store:
 				<span className={styles.userInfos}>
-					{userInfos.token /* Affichez le token du store */}
+					{userInfos.token }
 				</span>
 			</h4>
 		</div>
