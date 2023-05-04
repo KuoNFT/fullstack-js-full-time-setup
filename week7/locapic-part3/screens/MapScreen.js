08da96244f62940aa3de94e21a08036742343bf7
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-05-04 12:36:49 +0200

    locapic

--- week7/locapic-part3/screens/MapScreen.js
+++ week7/locapic-part3/screens/MapScreen.js
@@ -1,165 +1,123 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, TextInput, Button, Alert, Modal } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { addPlace } from '../reducers/user';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
-import { useSelector, useDispatch } from 'react-redux';
-import { addPlace } from '../reducers/user';
 
 export default function MapScreen() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  const places = useSelector((state) => state.user.value.places);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [tempCoordinates, setTempCoordinates] = useState(null);
-  const [poiName, setPoiName] = useState('');
-  const dispatch = useDispatch();
+ const dispatch = useDispatch();
+ const user = useSelector((state) => state.user.value);
 
-  const handleLongPress = (event) => {
-    setTempCoordinates(event.nativeEvent.coordinate);
-    setModalVisible(true);
-  };
+ const [currentPosition, setCurrentPosition] = useState(null);
+ const [tempCoordinates, setTempCoordinates] = useState(null);
+ const [modalVisible, setModalVisible] = useState(false);
+ const [newPlace, setNewPlace] = useState('');
 
-  const placeMarkers = places.map((place) => (
-    <Marker
-      key={place.name}
-      coordinate={{ latitude: place.latitude, longitude: place.longitude }}
-      title={place.name}
-    />
-  ));
+ useEffect(() => {
+   (async () => {
+     const { status } = await Location.requestForegroundPermissionsAsync();
 
-  const handleSubmit = (coordinates, name) => {
-    if (name.trim() === '') {
-      Alert.alert('Erreur', 'Veuillez entrer un nom pour le point d\'intérêt.');
-      return;
-    }
+     if (status === 'granted') {
+       Location.watchPositionAsync({ distanceInterval: 10 },
+         (location) => {
+           setCurrentPosition(location.coords);
+         });
+     }
+   })();
+ }, []);
 
-    const newPlace = {
-      name,
-      latitude: coordinates.latitude,
-      longitude: coordinates.longitude,
-    };
+ const handleLongPress = (e) => {
+   setTempCoordinates(e.nativeEvent.coordinate);
+   setModalVisible(true);
+ };
 
-    dispatch(addPlace(newPlace));
-    setPoiName('');
-  };
+ const handleNewPlace = () => {
+   dispatch(addPlace({ name: newPlace, latitude: tempCoordinates.latitude, longitude: tempCoordinates.longitude }));
+   setModalVisible(false);
+   setNewPlace('');
+ };
 
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
+ const handleClose = () => {
+   setModalVisible(false);
+   setNewPlace('');
+ };
 
-      const watchPosition = await Location.watchPositionAsync(
-        {
-          accuracy: Location.Accuracy.High,
-          distanceInterval: 10,
-        },
-        (newLocation) => {
-          setLocation(newLocation);
-        },
-        (error) => console.log(error)
-      );
+  const markers = user.places.map((data, i) => {
+   return <Marker key={i} coordinate={{ latitude: data.latitude, longitude: data.longitude }} title={data.name} />;
+ });
 
-      return () => {
-        if (watchPosition) {
-          watchPosition.remove();
-        }
-      };
-    })();
-  }, []);
 
   return (
+
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        mapType="hybrid"
-        initialRegion={{
-          latitude: 48.856614,
-          longitude: 2.3522219,
-          latitudeDelta: 10,
-          longitudeDelta: 10,
-        }}
-        onLongPress={handleLongPress}
-      >
-        {location && (
-          <Marker
-            coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
-            }}
-            title="Ma position"
-            pinColor="#fecb2d"
-          />
-        )}
-        {placeMarkers}
-      </MapView>
-      <Modal animationType="slide" transparent={true} visible={modalVisible}>
-        <View style={styles.modalView}>
-          <View style={styles.modalContent}>
-            <TextInput
-              style={styles.input}
-              placeholder="Nom du point d'intérêt"
-              value={poiName}
-              onChangeText={setPoiName}
-            />
-            <View style={styles.buttonContainer}>
-              <Button
-                title="Ajouter"
-                color="#ec6e5b"
-                onPress={() => {
-                  handleSubmit(tempCoordinates, poiName);
-                  setModalVisible(false);
-                }}
-              />
-              <View style={{ width: 10 }} />
-              <Button
-                title="Annuler"
-                color="#ec6e5b"
-                onPress={() => {
-              setModalVisible(false);
-              setPoiName('');
-            }}
-          />
-        </View>
-      </View>
-    </View>
-  </Modal>
-</View>
-);
+     <Modal visible={modalVisible} animationType="fade" transparent>
+       <View style={styles.centeredView}>
+         <View style={styles.modalView}>
+           <TextInput placeholder="New place" onChangeText={(value) => setNewPlace(value)} value={newPlace} style={styles.input} />
+           <TouchableOpacity onPress={() => handleNewPlace()} style={styles.button} activeOpacity={0.8}>
+             <Text style={styles.textButton}>Add</Text>
+           </TouchableOpacity>
+           <TouchableOpacity onPress={() => handleClose()} style={styles.button} activeOpacity={0.8}>
+             <Text style={styles.textButton}>Close</Text>
+           </TouchableOpacity>
+         </View>
+       </View>
+     </Modal>
+
+     <MapView onLongPress={(e) => handleLongPress(e)} mapType="hybrid" style={styles.map}>
+       {currentPosition && <Marker coordinate={currentPosition} title="My position" pinColor="#fecb2d" />}
+       {markers}
+     </MapView>
+   </View>
+
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
-flex: 1,
-},
-map: {
-width: '100%',
-height: '100%',
-},
-modalView: {
-flex: 1,
-justifyContent: 'center',
-alignItems: 'center',
-backgroundColor: 'rgba(0, 0, 0, 0.4)',
-},
-modalContent: {
-backgroundColor: 'white',
-borderRadius: 20,
-padding: 20,
-alignItems: 'center',
-width: '80%',
-},
-input: {
-width: '100%',
-borderColor: '#ccc',
-borderWidth: 1,
-borderRadius: 5,
-padding: 5,
-marginBottom: 20,
-},
-buttonContainer: {
-flexDirection: 'row',
-},
+ container: {
+   flex: 1,
+ },
+ map: {
+   flex: 1,
+ },
+ centeredView: {
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+ },
+ modalView: {
+   backgroundColor: 'white',
+   borderRadius: 20,
+   padding: 30,
+   alignItems: 'center',
+   shadowColor: '#000',
+   shadowOffset: {
+     width: 0,
+     height: 2,
+   },
+   shadowOpacity: 0.25,
+   shadowRadius: 4,
+   elevation: 5,
+ },
+ input: {
+   width: 150,
+   borderBottomColor: '#ec6e5b',
+   borderBottomWidth: 1,
+   fontSize: 16,
+ },
+ button: {
+   width: 150,
+   alignItems: 'center',
+   marginTop: 20,
+   paddingTop: 8,
+   backgroundColor: '#ec6e5b',
+   borderRadius: 10,
+ },
+ textButton: {
+   color: '#ffffff',
+   height: 24,
+   fontWeight: '600',
+   fontSize: 15,
+ },
 });
