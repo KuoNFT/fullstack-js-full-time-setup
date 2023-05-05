618ade7b42088aab63241f2b59ef4cda39f664b7
Valentin Lemaitre <79985088+KuoNFT@users.noreply.github.com>
2023-05-05 13:20:43 +0200

    eval

--- week7/evaluation-week7/App.js
+++ week7/evaluation-week7/App.js
@@ -1,64 +1,103 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Pressable, SafeAreaView } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
+  const [placeName, setPlaceName] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
   const [markers, setMarkers] = useState([]);
-  const titleInput = useRef();
-  const latInput = useRef();
-  const lonInput = useRef();
 
-  const addMarker = (title, latitude, longitude) => {
-    setMarkers([...markers, { title, latitude, longitude }]);
+  const handlePress = () => {
+    if (placeName && latitude && longitude) {
+      const newMarker = {
+        title: placeName,
+        coordinate: {
+          latitude: parseFloat(latitude),
+          longitude: parseFloat(longitude),
+        },
+      };
+      setMarkers([...markers, newMarker]);
+      setPlaceName('');
+      setLatitude('');
+      setLongitude('');
+    }
+  };
+
+  const addTestMarkers = () => {
+    const London = { lat: 51.507359, lon: -0.136439 };
+    const Madrid = { lat: 40.416775, lon: -3.703790 };
+    const Rome = { lat: 41.902783, lon: 12.496366 };
+
+    setMarkers([
+      ...markers,
+      {
+        title: 'London',
+        coordinate: {
+          latitude: London.lat,
+          longitude: London.lon,
+        },
+      },
+      {
+        title: 'Madrid',
+        coordinate: {
+          latitude: Madrid.lat,
+          longitude: Madrid.lon,
+        },
+      },
+      {
+        title: 'Rome',
+        coordinate: {
+          latitude: Rome.lat,
+          longitude: Rome.lon,
+        },
+      },
+    ]);
   };
 
   return (
     <View>
       <SafeAreaView style={styles.inputSection}>
         <TextInput
-          ref={titleInput}
           placeholder='Place Name'
           style={styles.input}
+          value={placeName}
+          onChangeText={setPlaceName}
         />
         <TextInput
-          ref={latInput}
           placeholder='Latitude'
           style={styles.input}
+          value={latitude}
+          onChangeText={setLatitude}
+          keyboardType='numeric'
         />
         <TextInput
-          ref={lonInput}
           placeholder='Longitude'
           style={styles.input}
+          value={longitude}
+          onChangeText={setLongitude}
+          keyboardType='numeric'
         />
-        <Pressable
-          style={styles.button}
-          onPress={() => {
-            addMarker(
-              titleInput.current.value,
-              parseFloat(latInput.current.value),
-              parseFloat(lonInput.current.value)
-            );
-          }}
-        >
+        <Pressable style={styles.button} onPress={handlePress}>
           <Text style={styles.buttonText}>Go</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={addTestMarkers}>
+          <Text style={styles.buttonText}>Ajouter les villes de test</Text>
         </Pressable>
       </SafeAreaView>
       <MapView
         initialRegion={{
-          latitude: 5,
-          longitude: 5,
-          latitudeDelta: 0.5,
-          longitudeDelta: 0.5,
+          latitude: 48.85,
+          longitude: 2.35,
+          latitudeDelta: 5,
+          longitudeDelta: 5,
         }}
         style={styles.map}
       >
         {markers.map((marker, index) => (
           <Marker
             key={index}
-            coordinate={{
-              latitude: marker.latitude,
-              longitude: marker.longitude,
-            }}
+            coordinate={marker.coordinate}
             title={marker.title}
           />
         ))}
@@ -87,7 +126,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     width: '80%',
     borderColor: 'gray',
-    paddingHorizontal: 20,
+    paddingHorizontal: 20, 
     paddingVertical: 5,
     margin: 5,
     borderRadius: 10,
